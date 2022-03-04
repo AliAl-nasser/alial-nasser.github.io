f2b49377cdfc4e7f4868cfc079593b08558d6652
alihalna100 <ali.al-nasser1@hotmail.com>
2022-03-04 19:35:34 +0100

    update

---
+++ background.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+    el: '#vantajs', 
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x482336,
+    midtoneColor: 0x41373b,
+    lowlightColor: 0x22228e,
+    baseColor: 0x141414,
+    speed: 3.0,
+    zoom: 1.50
+    })
