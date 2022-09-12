b17683da9fad5ce0df74877424cb3d4ee1a78607
alihalna100 <ali.al-nasser1@hotmail.com>
2022-09-12 13:57:17 +0200

    new site

---
+++ background.js
@@ -0,0 +1,13 @@
+VANTA.TOPOLOGY ({
+    el: '#vantajs', 
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x20b2aa,
+    backgroundColor: 0x1d1d1d
+    })
+    
